617f58c2856b26110d698d11810ad997a23f98b7
kolydart <kolydart@macbeth.gateweb.gr>
2025-02-25 19:47:21 +0200

    improve composer.json and psr4 autoload

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kolydart\Laravel\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+
+} 
