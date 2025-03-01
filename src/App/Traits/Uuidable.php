12facb4b186f7e71050d59adf4a328606ae47233
kolydart <kolydart@macbeth.gateweb.gr>
2025-03-01 13:53:17 +0200

    Uuidable trait

---
+++ src/App/Traits/Uuidable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kolydart\Laravel\App\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * Auto fill uuid column on model creation
+ */
+trait Uuidable
+{
+    public static function bootUuidable()
+    {
+        self::creating(function (Model $model) {
+            $model->uuid = (string) Str::uuid();
+        }); 
+    }
+} 
---
+++ tests/App/Traits/UuidableTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kolydart\Laravel\Tests\App\Traits;
+
+use Kolydart\Laravel\Tests\TestCase;
+use Kolydart\Laravel\App\Traits\Uuidable;
+use Illuminate\Database\Eloquent\Model;
+
+class UuidableTest extends TestCase
+{
+    /** @test */
+    public function it_exists()
+    {
+        $this->assertTrue(trait_exists(Uuidable::class));
+    }
+
+    // More specific tests would require a Laravel environment with database
+} 
