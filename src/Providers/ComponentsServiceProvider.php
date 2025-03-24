414267e48c5b8f3becfa1803a5943fa690fd3b76
kolydart <kolydart@macbeth.gateweb.gr>
2025-03-24 17:13:15 +0200

    feat: add Laravel version check for component registration

--- src/Providers/ComponentsServiceProvider.php
+++ src/Providers/ComponentsServiceProvider.php
@@ -8,14 +8,17 @@ use Illuminate\Support\Facades\Blade;
 /**
  * Service provider for registering Blade components.
  * 
- 
+ * Components are only registered for Laravel 8.0 and above.
  */
 class ComponentsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Register components
-        Blade::componentNamespace('Kolydart\\Laravel\\App\\View\\Components', 'kolydart');
+        // Only register components for Laravel 8+
+        if (version_compare($this->app->version(), '8.0.0', '>=')) {
+            // Register components
+            Blade::componentNamespace('Kolydart\\Laravel\\App\\View\\Components', 'kolydart');
+        }
     }
 
     public function register()
