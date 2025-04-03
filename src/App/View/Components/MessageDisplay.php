2a1abdfb2bda41c4b4cfe8a1ce79256031656317
kolydart <kolydart@macbeth.gateweb.gr>
2025-04-03 16:06:53 +0300

    move message-display view from external file into view render

--- src/App/View/Components/MessageDisplay.php
+++ src/App/View/Components/MessageDisplay.php
@@ -46,12 +46,27 @@ class MessageDisplay extends Component
         }
     }
 
-    public function render()
+    public function render(): string|null
     {
         if (empty($this->messages)) {
-            return '';
+            return null;
         }
 
-        return View::make('components.message-display');
+        return <<<'BLADE'
+            @foreach($messages as $message)
+                <div class="row mb-2">
+                    <div class="col-lg-12">
+                        <div class="alert alert-{{ $message['type'] }} {{ $message['type'] === 'error' ? 'text-white' : '' }}" role="alert">
+                            @if($message['type'] === 'warning' || $message['type'] === 'error')
+                                {{ $message['text'] }}
+                            @else
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                {{ $message['text'] }}
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        BLADE;
     }
 }
